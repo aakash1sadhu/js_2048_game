18ac1d21aea5881e9d3acc2bb5031bcaec293d7a
Aakash Sadhu <aakashsadhu@hotmail.com>
2026-04-17 12:45:50 +0100

    solution

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -21,24 +21,42 @@ class Game {
    * initial state.
    */
   constructor(initialState) {
-    // eslint-disable-next-line no-console
-    console.log(initialState);
+    this.size = 4;
+    this.score = 0;
+    this.status = 'idle';
+    this.board = initialState || this.createEmptyBoard();
   }
 
-  moveLeft() {}
-  moveRight() {}
-  moveUp() {}
-  moveDown() {}
+  moveLeft() {
+    this.move((row) => row);
+  }
+  moveRight() {
+    this.move((row) => row.reverse(), true);
+  }
+  moveUp() {
+    this.transpose();
+    this.moveLeft();
+    this.transpose();
+  }
+  moveDown() {
+    this.transpose();
+    this.moveRight();
+    this.transpose();
+  }
 
   /**
    * @returns {number}
    */
-  getScore() {}
+  getScore() {
+    return this.score;
+  }
 
   /**
    * @returns {number[][]}
    */
-  getState() {}
+  getState() {
+    return this.board;
+  }
 
   /**
    * Returns the current game status.
@@ -50,19 +68,142 @@ class Game {
    * `win` - the game is won;
    * `lose` - the game is lost
    */
-  getStatus() {}
+  getStatus() {
+    return this.status;
+  }
 
   /**
    * Starts the game.
    */
-  start() {}
+  start() {
+    this.status = 'playing';
+    this.addRandomTile();
+    this.addRandomTile();
+  }
 
   /**
    * Resets the game.
    */
-  restart() {}
+  restart() {
+    this.score = 0;
+    this.status = 'playing';
+    this.board = this.createEmptyBoard();
+    this.addRandomTile();
+    this.addRandomTile();
+  }
 
-  // Add your own methods here
+  createEmptyBoard() {
+    return Array.from({ length: this.size }, () => Array(this.size).fill(0));
+  }
+
+  addRandomTile() {
+    const emptyCells = [];
+
+    for (let r = 0; r < this.size; r++) {
+      for (let c = 0; c < this.size; c++) {
+        if (this.board[r][c] === 0) {
+          emptyCells.push([r, c]);
+        }
+      }
+    }
+
+    if (emptyCells.length === 0) {
+      return;
+    }
+
+    const [row, col] =
+      emptyCells[Math.floor(Math.random() * emptyCells.length)];
+
+    this.board[row][col] = Math.random() < 0.1 ? 4 : 2;
+  }
+
+  move(transformRow, reverse = false) {
+    let moved = false;
+
+    for (let r = 0; r < this.size; r++) {
+      let row = [...this.board[r]];
+
+      if (transformRow) {
+        row = transformRow([...row]);
+      }
+
+      const newRow = this.compress(row);
+
+      if (reverse) {
+        newRow.reverse();
+      }
+
+      if (JSON.stringify(newRow) !== JSON.stringify(this.board[r])) {
+        moved = true;
+        this.board[r] = newRow;
+      }
+    }
+
+    if (moved) {
+      this.addRandomTile();
+      this.checkGameStatus();
+    }
+  }
+
+  compress(row) {
+    let filtered = row.filter((val) => val !== 0);
+
+    for (let i = 0; i < filtered.length - 1; i++) {
+      if (filtered[i] === filtered[i + 1]) {
+        filtered[i] *= 2;
+        this.score += filtered[i];
+        filtered[i + 1] = 0;
+      }
+    }
+
+    filtered = filtered.filter((val) => val !== 0);
+
+    while (filtered.length < this.size) {
+      filtered.push(0);
+    }
+
+    return filtered;
+  }
+
+  transpose() {
+    this.board = this.board[0].map((_, c) => this.board.map((row) => row[c]));
+  }
+
+  checkGameStatus() {
+    for (let r = 0; r < this.size; r++) {
+      for (let c = 0; c < this.size; c++) {
+        if (this.board[r][c] === 2048) {
+          this.status = 'win';
+
+          return;
+        }
+      }
+    }
+
+    if (!this.canMove()) {
+      this.status = 'lose';
+    }
+  }
+
+  canMove() {
+    for (let r = 0; r < this.size; r++) {
+      for (let c = 0; c < this.size; c++) {
+        if (this.board[r][c] === 0) {
+          return true;
+        }
+
+        if (c < this.size - 1 && this.board[r][c] === this.board[r][c + 1]) {
+          return true;
+        }
+
+        if (r < this.size - 1 && this.board[r][c] === this.board[r + 1][c]) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
 }
 
 module.exports = Game;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,83 @@
 'use strict';
 
 // Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+import Game from '../modules/Game.class';
 
 // Write your code here
+const game = new Game();
+
+const startBtn = document.querySelector('.button');
+const scoreEl = document.querySelector('.game-score');
+const cells = document.querySelectorAll('.field-cell');
+const msgStart = document.querySelector('.message-start');
+const msgWin = document.querySelector('.message-win');
+const msgLose = document.querySelector('.message-lose');
+
+function render() {
+  const state = game.getState();
+
+  scoreEl.textContent = game.getScore();
+
+  cells.forEach((cell, i) => {
+    const row = Math.floor(i / 4);
+    const col = i % 4;
+    const value = state[row][col];
+
+    cell.textContent = value === 0 ? '' : value;
+    cell.className = 'field-cell';
+
+    if (value > 0) {
+      cell.classList.add(`field-cell--${value}`);
+    }
+  });
+
+  msgStart.classList.add('hidden');
+  msgWin.classList.add('hidden');
+  msgLose.classList.add('hidden');
+
+  if (game.getStatus() === 'win') {
+    msgWin.classList.remove('hidden');
+  } else if (game.getStatus() === 'lose') {
+    msgLose.classList.remove('hidden');
+  }
+}
+
+startBtn.addEventListener('click', () => {
+  if (game.getStatus() === 'idle') {
+    game.start();
+  } else {
+    game.restart();
+  }
+  render();
+});
+
+document.addEventListener('keydown', (ev) => {
+  if (game.getStatus() !== 'playing') {
+    return;
+  }
+
+  switch (ev.key) {
+    case 'ArrowLeft':
+      game.moveLeft();
+      break;
+    case 'ArrowRight':
+      game.moveRight();
+      break;
+    case 'ArrowUp':
+      game.moveUp();
+      break;
+    case 'ArrowDown':
+      game.moveDown();
+      break;
+    default:
+      return;
+  }
+
+  if (startBtn.classList.contains('start')) {
+    startBtn.textContent = 'Restart';
+    startBtn.classList.remove('start');
+    startBtn.classList.add('restart');
+  }
+
+  render();
+});
